9454976e87033a079260520abc1bf7368f2fee0b
Artyom Azarevich <temafilt@gmail.com>
2021-03-04 00:12:07 +0300

    Extract strip method for cleaning up stdout

--- functions/win32.py
+++ functions/win32.py
@@ -1,15 +1,18 @@
 import subprocess
 
 
+def strip(string):
+    string = string.split("\n", 1)[1]
+    string = string.replace("\n", "")
+    return string
+
+
 def get_wmic(device, name):
     command = "wmic " + device + " get " + name
-    result = str(subprocess.check_output(command, text=True))
-    result = result.split("\n", 1)[1]
-    result = result.replace("\n", "")
-    return result
+    stdout = str(subprocess.check_output(command, text=True))
+    return strip(stdout)
 
 
 def win32_gpu(name):
     device = "path win32_VideoController"
-    result = get_wmic(device, name)
-    return result
+    return get_wmic(device, name)
