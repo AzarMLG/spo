0438090bdc9c482c469f3dfe4c84bf238874b752
Artyom Azarevich <temafilt@gmail.com>
2021-03-04 16:57:06 +0300

    Refactor multiplatform network info, ps, uptime & who

--- spo/logic.py
+++ spo/logic.py
@@ -1,15 +1,8 @@
 import sys
-import time
-import socket
-import psutil
 
-from datetime import datetime, date
-from psutil._common import bytes2human
-from psutil._compat import get_terminal_size
-
+from spo.multiplatform.getinfo import print_partitions, print_network, print_ps, print_uptime, print_who
 from spo.linux.getinfo import linux_print_cpu, linux_print_bios, linux_print_disks, linux_print_mb, linux_print_gpu, \
     linux_print_monitor
-from spo.multiplatform.getinfo import print_partitions
 from spo.win32.getinfo import win32_print_cpu, win32_print_bios, win32_print_disks, win32_print_mb, win32_print_gpu, \
     win32_print_monitor
 
@@ -71,114 +64,16 @@ def info_monitor():
 
 
 def info_network():
-    af_map = {
-        socket.AF_INET: 'IPv4',
-        socket.AF_INET6: 'IPv6',
-        psutil.AF_LINK: 'MAC',
-    }
-    duplex_map = {
-        psutil.NIC_DUPLEX_FULL: "full",
-        psutil.NIC_DUPLEX_HALF: "half",
-        psutil.NIC_DUPLEX_UNKNOWN: "?",
-    }
-
-    stats = psutil.net_if_stats()
-    io_counters = psutil.net_io_counters(pernic=True)
-    for nic, addresses in psutil.net_if_addrs().items():
-        print("%s:" % nic)
-        if nic in stats:
-            st = stats[nic]
-            print("    Параметры      : ", end='')
-            print("скорость=%sMB, дуплекс=%s, mtu=%s, up=%s" % (
-                st.speed, duplex_map[st.duplex], st.mtu,
-                "yes" if st.isup else "no"))
-        if nic in io_counters:
-            io = io_counters[nic]
-            print("    Входящие       : ", end='')
-            print("байт=%s, пакет=%s, ошиб=%s, отброш=%s" % (
-                bytes2human(io.bytes_recv), io.packets_recv, io.errin,
-                io.dropin))
-            print("    Исходящие      : ", end='')
-            print("байт=%s, пакет=%s, ошиб=%s, отброш=%s" % (
-                bytes2human(io.bytes_sent), io.packets_sent, io.errout,
-                io.dropout))
-        for address in addresses:
-            print("    %-4s" % af_map.get(address.family, address.family), end="")
-            print(" Адрес     : %s" % address.address)
-            if address.broadcast:
-                print("         Трансляция: %s" % address.broadcast)
-            if address.netmask:
-                print("    Маска подсети  : %s" % address.netmask)
-            if address.ptp:
-                print("      p2p       : %s" % address.ptp)
-        print("")
+    print_network()
 
 
 def info_ps():
-    today_day = date.today()
-    temp_line = "%-10s %9s %9s %9s %9s %9s  %s"
-    attrs = ['pid', 'memory_percent', 'name', 'cmdline', 'cpu_times',
-             'create_time', 'memory_info', 'status', 'nice', 'username']
-    print(temp_line % ("Пользователь", "PID", "%Память",
-                       "Состояние", "Запущен", "Время", "Комманда"))
-    for p in psutil.process_iter(attrs, ad_value=None):
-        if p.info['create_time']:
-            create_time = datetime.fromtimestamp(p.info['create_time'])
-            if create_time.date() == today_day:
-                create_time = create_time.strftime("%H:%M")
-            else:
-                create_time = create_time.strftime("%b%d")
-        else:
-            create_time = ''
-        if p.info['cpu_times']:
-            cpu_time = time.strftime("%M:%S",
-                                     time.localtime(sum(p.info['cpu_times'])))
-        else:
-            cpu_time = ''
-
-        user = p.info['username']
-        if not user and psutil.POSIX:
-            try:
-                user = p.uids()[0]
-            except psutil.Error:
-                pass
-        if user and psutil.WINDOWS and '\\' in user:
-            user = user.split('\\')[1]
-        if not user:
-            user = ''
-        user = user[:9]
-        memp = round(p.info['memory_percent'], 1) if \
-            p.info['memory_percent'] is not None else ''
-        if p.info['cmdline']:
-            cmdline = ' '.join(p.info['cmdline'])
-        else:
-            cmdline = p.info['name']
-        status = p.info['status'][:5] if p.info['status'] else ''
-
-        line = temp_line % (
-            user,
-            p.info['pid'],
-            memp,
-            status,
-            create_time,
-            cpu_time,
-            cmdline)
-        print(line[:get_terminal_size()[0]])
+    print_ps()
 
 
 def info_uptime():
-    print("ПК был запущен:",
-          datetime.fromtimestamp(psutil.boot_time()).strftime("%d.%m.%Y %H:%M:%S"))
+    print_uptime()
 
 
 def info_who():
-    users = psutil.users()
-    for user in users:
-        proc_name = psutil.Process(user.pid).name() if user.pid else ""
-        print("%-12s %-10s %-10s %-14s %s" % (
-            user.name,
-            user.terminal or '-',
-            datetime.fromtimestamp(user.started).strftime("%Y-%m-%d %H:%M"),
-            "(%s)" % user.host if user.host else "",
-            proc_name
-        ))
+    print_who()
--- spo/multiplatform/getinfo.py
+++ spo/multiplatform/getinfo.py
@@ -1,4 +1,8 @@
+import socket
 import sys
+import time
+from datetime import date, datetime
+from shutil import get_terminal_size
 
 import psutil
 from psutil._common import bytes2human
@@ -23,3 +27,116 @@ def print_partitions():
             int(usage.percent),
             part.fstype,
             part.mountpoint))
+
+
+def print_network():
+    af_map = {
+        socket.AF_INET: 'IPv4',
+        socket.AF_INET6: 'IPv6',
+        psutil.AF_LINK: 'MAC',
+    }
+    duplex_map = {
+        psutil.NIC_DUPLEX_FULL: "full",
+        psutil.NIC_DUPLEX_HALF: "half",
+        psutil.NIC_DUPLEX_UNKNOWN: "?",
+    }
+    stats = psutil.net_if_stats()
+    io_counters = psutil.net_io_counters(pernic=True)
+    for nic, addresses in psutil.net_if_addrs().items():
+        print("%s:" % nic)
+        if nic in stats:
+            st = stats[nic]
+            print("    Параметры      : ", end='')
+            print("скорость=%sMB, дуплекс=%s, mtu=%s, up=%s" % (
+                st.speed, duplex_map[st.duplex], st.mtu,
+                "yes" if st.isup else "no"))
+        if nic in io_counters:
+            io = io_counters[nic]
+            print("    Входящие       : ", end='')
+            print("байт=%s, пакет=%s, ошиб=%s, отброш=%s" % (
+                bytes2human(io.bytes_recv), io.packets_recv, io.errin,
+                io.dropin))
+            print("    Исходящие      : ", end='')
+            print("байт=%s, пакет=%s, ошиб=%s, отброш=%s" % (
+                bytes2human(io.bytes_sent), io.packets_sent, io.errout,
+                io.dropout))
+        for address in addresses:
+            print("    %-4s" % af_map.get(address.family, address.family), end="")
+            print(" Адрес     : %s" % address.address)
+            if address.broadcast:
+                print("         Трансляция: %s" % address.broadcast)
+            if address.netmask:
+                print("    Маска подсети  : %s" % address.netmask)
+            if address.ptp:
+                print("      p2p       : %s" % address.ptp)
+        print("")
+
+
+def print_ps():
+    today_day = date.today()
+    temp_line = "%-10s %9s %9s %9s %9s %9s  %s"
+    attrs = ['pid', 'memory_percent', 'name', 'cmdline', 'cpu_times',
+             'create_time', 'memory_info', 'status', 'nice', 'username']
+    print(temp_line % ("Пользователь", "PID", "%Память",
+                       "Состояние", "Запущен", "Время", "Комманда"))
+    for p in psutil.process_iter(attrs, ad_value=None):
+        if p.info['create_time']:
+            create_time = datetime.fromtimestamp(p.info['create_time'])
+            if create_time.date() == today_day:
+                create_time = create_time.strftime("%H:%M")
+            else:
+                create_time = create_time.strftime("%b%d")
+        else:
+            create_time = ''
+        if p.info['cpu_times']:
+            cpu_time = time.strftime("%M:%S",
+                                     time.localtime(sum(p.info['cpu_times'])))
+        else:
+            cpu_time = ''
+
+        user = p.info['username']
+        if not user and psutil.POSIX:
+            try:
+                user = p.uids()[0]
+            except psutil.Error:
+                pass
+        if user and psutil.WINDOWS and '\\' in user:
+            user = user.split('\\')[1]
+        if not user:
+            user = ''
+        user = user[:9]
+        memp = round(p.info['memory_percent'], 1) if \
+            p.info['memory_percent'] is not None else ''
+        if p.info['cmdline']:
+            cmdline = ' '.join(p.info['cmdline'])
+        else:
+            cmdline = p.info['name']
+        status = p.info['status'][:5] if p.info['status'] else ''
+
+        line = temp_line % (
+            user,
+            p.info['pid'],
+            memp,
+            status,
+            create_time,
+            cpu_time,
+            cmdline)
+        print(line[:get_terminal_size()[0]])
+
+
+def print_uptime():
+    print("ПК был запущен:",
+          datetime.fromtimestamp(psutil.boot_time()).strftime("%d.%m.%Y %H:%M:%S"))
+
+
+def print_who():
+    users = psutil.users()
+    for user in users:
+        proc_name = psutil.Process(user.pid).name() if user.pid else ""
+        print("%-12s %-10s %-10s %-14s %s" % (
+            user.name,
+            user.terminal or '-',
+            datetime.fromtimestamp(user.started).strftime("%Y-%m-%d %H:%M"),
+            "(%s)" % user.host if user.host else "",
+            proc_name
+        ))
